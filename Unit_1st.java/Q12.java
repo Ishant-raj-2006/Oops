f5c072963a32df5190f66d277b630138cc6dc8af
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-03-07 15:34:19 +0530

    Today 07/03/2026 this is prime number checker

--- Unit_1st.java/Q12.java
+++ Unit_1st.java/Q12.java
@@ -1,20 +1,27 @@
-// WAP for find even or odd 
+// WAP for find prime number or note 
 
 
 import java.util.Scanner;
 
 
-public class Q11 {
+public class Q12 {
     public static void main(String[] args) {
-        int num ;
+        int num ,count = 0;
         Scanner sb = new Scanner(System.in);
-        System.out.print("Enter Number fgs = ");
+        System.out.print("Enter Number = ");
         num = sb.nextInt();
-        if(num%2 == 0){
-            System.out.print("Even number ");
+        for(int i= 1; i<=num; i++){
+            if(num%i==0){
+                count ++;
+            }
+        }
+        if(count ==2){
+            System.out.println("This is prime numebr ");
         }
         else{
-            System.out.println("odd number ");
+            System.out.println("This is  note prime numebr ");
         }
+
+        
     }
 }
