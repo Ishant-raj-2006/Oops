8a1e2d930fe43a05a83516bdd3b6eee4bada831e
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-03 12:14:31 +0530

    Q4

---
+++ Unit_2nd.java/Q4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        int[] arr = {21,5,67,9,90,45,23};
+        int idx, a, ans = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter index and value :- ");
+
+        try {
+            idx = sc.nextInt();
+            a = sc.nextInt();
+
+            ans = arr[idx] / a;
+            System.out.println("Ans = " + ans);
+
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Exception occurred: " + e);
+        }
+         catch(ArithmeticException e) {
+            System.out.println("Exception occurred: " + e);
+        }
+
+        System.out.println("Ans " + ans);
+    }
+}
