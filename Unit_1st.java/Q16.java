3ca0e912bb592f3a9e56f674e9a58bc73cd5e6f6
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-03 11:58:33 +0530

    Q2

---
+++ Unit_1st.java/Q16.java
@@ -0,0 +1,18 @@
+public class Q16 {
+    int eid;
+    String name;
+    String department;
+    //  function Overloading
+    void f1(){
+        System.out.println(" Zero ");
+    } 
+    void f1(int a){
+        System.out.println(" 1 Integer ");
+    } 
+    void f1(double  b){
+        System.out.println(" Zero ");
+    } 
+    public static void main(String[] args) {
+  
+    }
+}
---
+++ Unit_2nd.java/Q2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int[] arr ={21,5,67,9,90,45,23};
+        int idx,a,ans=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter index and value :-");
+        idx=sc.nextInt();
+        a=sc.nextInt();
+        ans = arr[idx]/a;
+        System.out.println("Ans="+ans);
+    }
+    catch(Exception e) {
+        System.out.println("Exception occurred : "+e);
+    }
+    System.out.print("Ans"+ans);
+}
+// catch try ke bina use nhi hota h , isliye iss me error aa raha h 
