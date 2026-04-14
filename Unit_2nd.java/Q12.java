3a196d4d4b9bab3342fef94e77501b1eaa441cc1
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-14 21:39:58 +0530

    Today 14/04/2026 , 1.1

--- Unit_2nd.java/Q12.java
+++ Unit_2nd.java/Q12.java
@@ -1,7 +1,10 @@
 
+import java.util.Scanner;
 
-// WAP for make selfe data type 
-public class Q11{
+
+
+// WAP for make selfe data type and take it's input from user.
+public class Q12{
   
         public static class Student{
             String name;
@@ -24,8 +27,16 @@ public static void main(String[] args) {
     s3.name = "Ishant Raj";
     s3.rno = 31;
     s3.cgpa = 4.67;
+    Scanner sc = new Scanner(System.in);
 
     System.out.println(s1.name+" "+s1.cgpa+" "+s1.rno);
+System.out.print("Enter Your name = ");
+s2.name = sc.next();
+System.out.print("Enter Your Roll Number  = ");
+s2.rno = sc.nextInt();
+System.out.print("Enter Your CGPA = ");
+s2.cgpa = sc.nextFloat();
+ System.out.println(s2.name+" "+s2.cgpa+" "+s2.rno);
 }
 }
 
