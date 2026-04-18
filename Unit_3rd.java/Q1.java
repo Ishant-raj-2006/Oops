986de5b522204335db55bc4ff8e2051c85ebf9cc
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-18 19:49:10 +0530

    Unit_3_Q_1.2

--- Unit_3rd.java/Q1.java
+++ Unit_3rd.java/Q1.java
@@ -1,6 +1,9 @@
 @FunctionalInterface
 interface MyInterface{
-     void myMethod();
+     void abrst();
+      static void myMethod(){
+          System.out.println("Hello Ishant");
+      }
 }
 // public class Q1{
 //     public static void main(String[] args){
