6907be53846f2b8202ea974a7b966c931a747ad0
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-22 01:31:11 +0530

    Opps 1.8

--- Oops_with_PW.java/Q4.java
+++ Oops_with_PW.java/Q4.java
@@ -3,11 +3,18 @@ class Student{
         String name = "Raj";
         private int roll = 31;
         double cgpa = 4.67;
-        private void print() {
+       void print() {
             System.out.println(name+" "+roll+" "+cgpa+"%");
         }
-        void p(){
-            print();
+        // void p(){
+        //     print();
+        // }
+        int getroll(){
+            // System.out.println(roll);
+            return roll;
+        }
+        void setroll(int x){ // Setter
+            roll =x;
         }
 
     }
@@ -17,8 +24,12 @@ public class Q4 {
         Student s1 = new Student();
         s1.cgpa =8.9;
         s1.name ="Ishant Raj";
-        s1.p();
+        s1.print();
          Student s2 = new Student();
-        s2.p();
+        s2.print();
+        System.out.println(s1.getroll());
+        
+        s1.setroll(337);
+        System.out.println(s1.getroll());
     }
 }
