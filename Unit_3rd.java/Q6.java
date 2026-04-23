bd3efa2550aeac867067e451a0871d30aa83ba63
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-23 23:14:46 +0530

    Today 23/04?2026 Opps Unit_3 , Q6

---
+++ Unit_3rd.java/Q6.java
@@ -0,0 +1,24 @@
+public class Q6{
+    public  static void main(String[] args){
+        
+        int num =5;
+        String dayName;
+        switch(num){
+            case 1:
+                System.out.println("This day 1");
+                break ;
+            case 2:
+                System.out.println("This day 2");
+                break ;
+            case 3:
+                System.out.println("This day 3");
+                break ;
+            case 4:
+                System.out.println("This day 4");
+                break ;
+                default:
+                    System.out.println("This is Invalid day");
+                    break;
+        }
+    }
+}
