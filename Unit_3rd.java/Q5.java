35066383d571dd7530f3cbf0b3d5194f4bc4f2d0
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-23 23:06:11 +0530

    Today 23/04/2026 , Oops Unit_3 Q5

---
+++ Unit_3rd.java/Q5.java
@@ -0,0 +1,12 @@
+// Static Method
+
+interface Q5 {
+    static void StaticMethod(){
+        System.out.println("Static method in interface.");
+    }
+}
+class StaticMethodInInterfaceExample{
+    public static void main(String[] args) {
+        Q5.StaticMethod();
+    }
+}
