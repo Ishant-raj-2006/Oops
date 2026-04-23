c60a05ae7d4988b910a07b90d53d17c6c20bd702
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-23 11:21:18 +0530

    Today 23/04/2026 , Oops 1.2

--- Oops_with_PW.java/Q7.java
+++ Oops_with_PW.java/Q7.java
@@ -15,6 +15,9 @@ public class Q7 {
         Pokemon p2 = new Pokemon();
         p2.name ="Charizarad"; p2.type = "Fire"; p2.attack =110;
         System.out.println(p1.type);
+        Pokemon p3 = p1; // P3 is shallow copy of p1 (can control).
+        p3.type = "Water";
+        System.out.println(p3.type);
         
     }
 }
