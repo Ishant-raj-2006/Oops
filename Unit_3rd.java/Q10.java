253c85ef59b5568b75cad9246e837454084f62d8
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-24 09:44:50 +0530

    Today Opps unit 3 Q10

---
+++ Unit_3rd.java/Q10.java
@@ -0,0 +1,8 @@
+public class Q10 {
+    public static void main(String[] args) {
+        var name = "Ishant";
+        var num = 95073;
+        System.out.println(name);
+        System.out.println(num);
+    }
+}
