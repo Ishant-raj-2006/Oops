2f0b6de0f088af0a4203c5078e4cabc166efb1be
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-24 09:38:44 +0530

    Today 24/04/2026 opps Unit_3 Q9

---
+++ Unit_3rd.java/Q9.java
@@ -0,0 +1,10 @@
+public class Q9 {
+     public static void main(String[] args) {
+         String textBlock ="""
+                   Hello My name is Ishant Raj
+               How are you 
+                    And aslo tell me who are who
+                   """;
+                   System.out.println(textBlock);
+     }
+}
