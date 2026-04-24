e38d0804ecc3ec1be2985110f72c63254d01a3d4
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-24 10:18:33 +0530

    Today 24/04/2026 , Unit 3 Q12

--- Unit_3rd.java/Q12.java
+++ Unit_3rd.java/Q12.java
@@ -1,5 +1,17 @@
+abstract class Calculator<T> {
+    abstract T add(T a, T b);
+}
+
 public class Q12 {
     public static void main(String[] args) {
-        
+
+        Calculator<Integer> obj1 = new Calculator<Integer>() {
+            @Override
+            Integer add(Integer a, Integer b) {
+                return a + b;
+            }
+        };
+
+        System.out.println(obj1.add(12, 23));
     }
 }
