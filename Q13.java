b648ed256fc695d2a7536c589be002fb7dfcc8be
Ishant-raj-2006 <ishant786raj@gmail.com>
2026-04-25 12:11:56 +0530

    Today 25/04/2026 , Opps unit 3 Q13

---
+++ Q13.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Q13{
+    public static void main(String[] args) {
+        List<String> items = Arrays.asList("Apple","banana", "Papaya", "Ishant Raj");
+        items.forEach(item -> System.out.println(item));
+    }
+}
